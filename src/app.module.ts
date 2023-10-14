dcc211a4f45186b894f5889cb734b917a92dbfac
Hatim Rachid <hat83203@gmail.com>
2023-10-14 21:31:08 +0100

    adding the ability to serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,16 @@ import { AchievementModule } from './res/achievement/achievement.module';
 import { MatchesModule } from './res/matches/matches.module';
 import { ChatModule } from './res/chat/chat.module';
 import { NotificationModule } from './res/notification/notification.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
-  imports: [ProfileModule, FriendModule, AchievementModule, MatchesModule, ChatModule, NotificationModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+	imports: [ServeStaticModule.forRoot({
+		rootPath: join(__dirname, '..', 'public'),
+		renderPath:"",
+	}), ProfileModule, FriendModule, AchievementModule, MatchesModule, ChatModule, NotificationModule],
+	controllers: [AppController],
+	providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
+
+
