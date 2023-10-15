4437945170d9292d1de359f1276342adbda91df8
med-ghali <tagemouati.medghali@gmail.com>
2023-10-15 15:34:18 +0100

    prisma fixes

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,30 +1,18 @@
 import { Injectable,UnauthorizedException  } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 import { HashingService } from '../hashing/hashing.service';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
-import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthenticationService {
 	constructor( private prisma: PrismaService, private hashingService : HashingService ) {}
 
 	async signUp(signUpDto : SignUpDto ) {
-		const user = new User()
-		user.email = signUpDto.email
-		user.password = await this.hashingService.hash(signUpDto.password)
-		return this.prisma.user.create({data:user})
+
 	}
 
 	async signIn (signInDto : SignInDto) {
-		const user = await this.prisma.user.findUnique({ where: {
-			email: signInDto.email
-		}})
-		if (!user)
-			throw new UnauthorizedException("user doesn't exist")
-		const isEquall = this.hashingService.compare(signInDto.password, user.password)
-		if (!isEquall)
-			throw new UnauthorizedException("wrong password")
-		return true
+
 	}
 }
--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HashingService } from './hashing/hashing.service';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 
 
 @Module({
-  providers: [HashingService, AuthenticationService],
+  providers: [HashingService, AuthenticationService, PrismaService],
   controllers: [AuthenticationController],
   imports: []
 })
