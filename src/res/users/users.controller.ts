cbe5a91ea571328cb045adceb1f746bae5baa3f1
Mohamed El Ghali Tagemouati <tagemouati.medghali@gmail.com>
2023-10-22 19:30:08 +0100

    finduserbyname

--- src/res/users/users.controller.ts
+++ src/res/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
@@ -33,5 +32,10 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
+  }
+
+  @Get('/list/:name')
+  findByName(@Param('name') name: string) {
+    return this.usersService.findByName(name);
   }
 }
