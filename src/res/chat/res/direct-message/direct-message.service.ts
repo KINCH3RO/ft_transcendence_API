fef02fe7e140a9350b25fe73533786ab1945b330
Hatim Rachid <hat83203@gmail.com>
2023-10-27 22:06:34 +0100

    direct messaging almost finished

--- src/res/chat/res/direct-message/direct-message.service.ts
+++ src/res/chat/res/direct-message/direct-message.service.ts
@@ -7,90 +7,89 @@ import { WebSocketService } from 'src/res/web-socket/web-socket.service';
 
 @Injectable()
 export class DirectMessageService {
-  constructor(
-    private prisma: PrismaService,
-    private webSocketService: WebSocketService,
-  ) {}
+	constructor(
+		private prisma: PrismaService,
+		private webSocketService: WebSocketService,
+	) { }
 
-  create(senderId: string, receiverId: string) {
-    return this.prisma.directMessage.create({
-      data: {
-        receiverID: receiverId,
-        senderID: senderId,
-        message: {},
-      },
-    });
-  }
+	create(senderId: string, receiverId: string) {
+		return this.prisma.directMessage.create({
+			data: {
+				receiverID: receiverId,
+				senderID: senderId,
+				message: {},
+			},
+		});
+	}
 
-  findYourDM(senderId: string) {
-    return this.prisma.directMessage.findMany({
-      where: { senderID: senderId },
-    });
-  }
+	findYourDM(senderId: string) {
+		return this.prisma.directMessage.findMany({
+			where: { senderID: senderId },
+		});
+	}
 
-  remove(DmId: string) {
-    return this.prisma.directMessage.delete({ where: { id: DmId } });
-  }
+	remove(DmId: string) {
+		return this.prisma.directMessage.delete({ where: { id: DmId } });
+	}
 
-  // findDMByReciverName
+	// findDMByReciverName
 
-  async listCurrentDM(userID: string) {
-    const dms: directMessage[] = await this.prisma.directMessage.findMany({
-      include: {
-        sender: {
-          select: {
-            avatarUrl: true,
-            userName: true,
-            id: true,
-            onlineStatus: true,
-            fullName: true,
-          },
-        },
-        receiver: {
-          select: {
-            avatarUrl: true,
-            userName: true,
-            id: true,
-            onlineStatus: true,
-            fullName: true,
-          },
-        },
-        message: {
-          take: 1,
-          orderBy: {
-            createdAt: 'desc',
-          },
-          select: {
-            senderID: true,
-            content: true,
-            attachment: true,
+	async listCurrentDM(userID: string) {
+		const dms: directMessage[] = await this.prisma.directMessage.findMany({
+			include: {
+				sender: {
+					select: {
+						avatarUrl: true,
+						userName: true,
+						id: true,
+						onlineStatus: true,
+						fullName: true,
+					},
+				},
+				receiver: {
+					select: {
+						avatarUrl: true,
+						userName: true,
+						id: true,
+						onlineStatus: true,
+						fullName: true,
+					},
+				},
+				message: {
+					take: 1,
+					orderBy: {
+						createdAt: 'desc',
+					},
+					select: {
+						senderID: true,
+						content: true,
+						attachment: true,
 						createdAt: true,
-            updatedAt: true
-          },
-        },
-      },
-      where: {
-        OR: [{ senderID: userID }, { receiverID: userID }],
-      },
-    });
+						updatedAt: true
+					},
+				},
+			},
+			where: {
+				OR: [{ senderID: userID }, { receiverID: userID }],
+			},
+		});
 
-    return dms.map((data: DirectMessage) => {
-      let baseData: DirectMessage = {
+		return dms.map((data: DirectMessage) => {
+			let baseData: DirectMessage = {
 				id: data.id,
 				receiverID: data.receiverID,
 				senderID: data.senderID,
 			};
 
-      if (userID != data.receiverID) baseData['friend'] = data.receiver;
-      if (userID != data.senderID) baseData['friend'] = data.sender;
-      baseData['isSender'] = userID == data.senderID;
-      baseData['friend'].onlineStatus = this.webSocketService.isOnline(
-        baseData['friend'].id,
-      );
-      baseData['message'] = data.message[0];
+			if (userID != data.receiverID) baseData['friend'] = data.receiver;
+			if (userID != data.senderID) baseData['friend'] = data.sender;
+			baseData['isSender'] = userID == data.senderID;
+			baseData['friend'].onlineStatus = this.webSocketService.isOnline(
+				baseData['friend'].id,
+			);
+			baseData['message'] = data.message[0];
 
-			console.log('>> data: ', baseData);
-      return baseData;
-    });
-  }
+			return baseData;
+		});
+	}
 }
