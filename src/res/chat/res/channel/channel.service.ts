86577ae4f004d1b133f2f566645f360142cbe358
rash0x6964 <rash0x6964@gmail.com>
2023-11-08 09:53:13 +0100

    fix a minor createChannel issue

--- src/res/chat/res/channel/channel.service.ts
+++ src/res/chat/res/channel/channel.service.ts
@@ -8,125 +8,130 @@ import { WebSocketService } from 'src/res/web-socket/services/web-socket.service
 
 @Injectable()
 export class ChannelService {
-	constructor(
-		private prisma: PrismaService,
-		private hashingService: HashingService,
-		private webSocketService: WebSocketService,
-	) { }
+  constructor(
+    private prisma: PrismaService,
+    private hashingService: HashingService,
+    private webSocketService: WebSocketService,
+  ) {}
 
-	async create(createChannelDto: CreateChannelDto, id: string) {
-		return this.prisma.channel.create({
-			select: {
-				id: true,
-				name: true,
-				imageUrl: true,
-				visibility: true,
-				message: {
-					take: 1,
-					orderBy: {
-						createdAt: 'desc',
-					},
-					select: {
-						senderID: true,
-						content: true,
-						attachment: true,
-						createdAt: true,
-						updatedAt: true,
-					},
-				},
-				channels: true,
-			},
-			data: {
-				imageUrl: createChannelDto.imageUrl,
-				name: createChannelDto.name,
-				visibility: createChannelDto.visibility,
-				password: createChannelDto.password
-					? await this.hashingService.hash(createChannelDto.password)
-					: null,
-				channels: {
-					create: {
-						userID: id,
-						role: 'OWNER',
-						status: 'FREE',
-					},
-				},
-				message: {},
-			},
-		});
-	}
+  async create(createChannelDto: CreateChannelDto, id: string) {
+    let room: any = await this.prisma.channel.create({
+      select: {
+        id: true,
+        name: true,
+        imageUrl: true,
+        visibility: true,
+        message: {
+          take: 1,
+          orderBy: {
+            createdAt: 'desc',
+          },
+          select: {
+            senderID: true,
+            content: true,
+            attachment: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        channels: true,
+      },
+      data: {
+        imageUrl: createChannelDto.imageUrl,
+        name: createChannelDto.name,
+        visibility: createChannelDto.visibility,
+        password: createChannelDto.password
+          ? await this.hashingService.hash(createChannelDto.password)
+          : null,
+        channels: {
+          create: {
+            userID: id,
+            role: 'OWNER',
+            status: 'FREE',
+          },
+        },
+        message: {},
+      },
+    });
 
-	findAll() {
-		return this.prisma.channel.findMany();
-	}
+    room['isMemeber'] = true;
+    room['owner'] = 'OWNER';
+		
+    return room;
+  }
 
-	async findOne(id: string) {
-		const channel = await this.prisma.channel.findUnique({
-			where: { id },
-			select: {
-				id: true,
-				visibility: true,
-				name: true,
-				imageUrl: true,
-				channels: {
-					select: {
-						user: {
-							select: {
-								id: true,
-								onlineStatus: true,
-							},
-						},
-					},
-				},
-			},
-		});
+  findAll() {
+    return this.prisma.channel.findMany();
+  }
 
-		channel.channels.map((item) => {
-			item.user.onlineStatus = this.webSocketService.isOnline(item.user.id);
-		});
+  async findOne(id: string) {
+    const channel = await this.prisma.channel.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        visibility: true,
+        name: true,
+        imageUrl: true,
+        channels: {
+          select: {
+            user: {
+              select: {
+                id: true,
+                onlineStatus: true,
+              },
+            },
+          },
+        },
+      },
+    });
 
-		return channel;
-	}
+    channel.channels.map((item) => {
+      item.user.onlineStatus = this.webSocketService.isOnline(item.user.id);
+    });
 
-	async update(updateChannelDto: UpdateChannelDto) {
-		return this.prisma.channel.update({
-			where: {
-				id: updateChannelDto.id,
-			},
-			data: {
-				imageUrl: updateChannelDto.imageUrl,
-				name: updateChannelDto.name,
-				password: await this.hashingService.hash(updateChannelDto.password),
-				visibility: updateChannelDto.visibility,
-			},
-		});
-	}
+    return channel;
+  }
 
-	async remove(userId: string, removeChannelDto: UpdateChannelDto) {
-		const actor: channelUser = await this.prisma.channelUser.findUnique({
-			where: {
-				userID_channelID: { channelID: removeChannelDto.id, userID: userId },
-			},
-		});
+  async update(updateChannelDto: UpdateChannelDto) {
+    return this.prisma.channel.update({
+      where: {
+        id: updateChannelDto.id,
+      },
+      data: {
+        imageUrl: updateChannelDto.imageUrl,
+        name: updateChannelDto.name,
+        password: await this.hashingService.hash(updateChannelDto.password),
+        visibility: updateChannelDto.visibility,
+      },
+    });
+  }
 
-		if (actor && actor.role != 'OWNER')
-			throw new HttpException('Nice try', HttpStatus.FORBIDDEN);
+  async remove(userId: string, removeChannelDto: UpdateChannelDto) {
+    const actor: channelUser = await this.prisma.channelUser.findUnique({
+      where: {
+        userID_channelID: { channelID: removeChannelDto.id, userID: userId },
+      },
+    });
 
-		const deleteMessages = this.prisma.message.deleteMany({
-			where: { channelID: removeChannelDto.id },
-		});
-		const deleteUsers = this.prisma.channelUser.deleteMany({
-			where: { channelID: removeChannelDto.id },
-		});
-		const deleteChannel = this.prisma.channel.delete({
-			where: { id: removeChannelDto.id },
-		});
+    if (actor && actor.role != 'OWNER')
+      throw new HttpException('Nice try', HttpStatus.FORBIDDEN);
 
-		return this.prisma.$transaction([
-			deleteMessages,
-			deleteUsers,
-			deleteChannel,
-		]);
-	}
+    const deleteMessages = this.prisma.message.deleteMany({
+      where: { channelID: removeChannelDto.id },
+    });
+    const deleteUsers = this.prisma.channelUser.deleteMany({
+      where: { channelID: removeChannelDto.id },
+    });
+    const deleteChannel = this.prisma.channel.delete({
+      where: { id: removeChannelDto.id },
+    });
+
+    return this.prisma.$transaction([
+      deleteMessages,
+      deleteUsers,
+      deleteChannel,
+    ]);
+  }
 
   async findChannelByName(currentUserId: string, name: string) {
     let list: any = await this.prisma.channel.findMany({
@@ -162,49 +167,45 @@ export class ChannelService {
       if (channel_user) {
         item['isMemeber'] = true;
         item['owner'] = channel_user.role;
-      } else
-        item['isMemeber'] = false;
+      } else item['isMemeber'] = false;
     });
-
-
 
     return list;
   }
 
-	async listCurrentUserChannel(currentUserId: string) {
-		let list: any = await this.prisma.channel.findMany({
-			where: { channels: { some: { userID: currentUserId } } },
-			select: {
-				id: true,
-				name: true,
-				imageUrl: true,
-				visibility: true,
-				message: {
-					take: 1,
-					orderBy: {
-						createdAt: 'desc',
-					},
-					select: {
-						senderID: true,
-						content: true,
-						attachment: true,
-						createdAt: true,
-						updatedAt: true,
-					},
-				},
-				channels: true,
-			},
-		});
+  async listCurrentUserChannel(currentUserId: string) {
+    let list: any = await this.prisma.channel.findMany({
+      where: { channels: { some: { userID: currentUserId } } },
+      select: {
+        id: true,
+        name: true,
+        imageUrl: true,
+        visibility: true,
+        message: {
+          take: 1,
+          orderBy: {
+            createdAt: 'desc',
+          },
+          select: {
+            senderID: true,
+            content: true,
+            attachment: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        channels: true,
+      },
+    });
 
     list.map((item) => {
       let channel_user = item.channels.find((x) => x.userID == currentUserId);
       if (channel_user) {
         item['isMemeber'] = true;
         item['owner'] = channel_user.role;
-      } else
-        item['isMemeber'] = false;
+      } else item['isMemeber'] = false;
     });
 
-		return list;
-	}
+    return list;
+  }
 }
