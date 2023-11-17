d86b0c54ae158a4d808efdbd417159985f66c86b
samini <soufianeamini2002@gmail.com>
2023-11-17 19:52:50 +0100

    added vs matches route for chat

--- src/res/match/match.service.ts
+++ src/res/match/match.service.ts
@@ -6,203 +6,225 @@ import { ActiveUserData } from 'src/iam/interfaces/active-user.interface';
 
 @Injectable()
 export class MatchService {
-	constructor(private readonly prismaService: PrismaService) { }
-	private readonly logger = new Logger(MatchService.name);
+  constructor(private readonly prismaService: PrismaService) {}
+  private readonly logger = new Logger(MatchService.name);
 
-	async create(createMatchDto: CreateMatchDto) {
-		const result = await this.prismaService.matches.create({
-			data: createMatchDto,
-		});
+  async create(createMatchDto: CreateMatchDto) {
+    const result = await this.prismaService.matches.create({
+      data: createMatchDto,
+    });
 
-		this.logger.debug(
-			`create Match ${result.winnerID} vs ${result.loserID} date: ${result.date}`,
-		);
-		return result;
-	}
+    return result;
+  }
 
-	async findAll(user: ActiveUserData) {
-		this.logger.log(`findAll for ${user.username}`);
+  async findAll(user: ActiveUserData) {
+    this.logger.log(`findAll for ${user.username}`);
 
-		const result = await this.prismaService.matches.findMany({
-			where: {
-				OR: [
-					{
-						winnerID: {
-							equals: user.sub,
-						},
-					},
-					{
-						loserID: {
-							equals: user.sub,
-						},
-					},
-				],
-			},
-		});
+    const result = await this.prismaService.matches.findMany({
+      where: {
+        OR: [
+          {
+            winnerID: {
+              equals: user.sub,
+            },
+          },
+          {
+            loserID: {
+              equals: user.sub,
+            },
+          },
+        ],
+      },
+    });
 
-		this.logger.verbose(result);
-		return result;
-	}
+    return result;
+  }
 
-	async findAllById(id: string) {
-		const result = await this.prismaService.matches.findMany({
-			where: {
-				OR: [
-					{
-						winnerID: {
-							equals: id,
-						},
-					},
-					{
-						loserID: {
-							equals: id,
-						},
-					},
-				],
-			},
-		});
+  async findAllById(id: string) {
+    const result = await this.prismaService.matches.findMany({
+      where: {
+        OR: [
+          {
+            winnerID: {
+              equals: id,
+            },
+          },
+          {
+            loserID: {
+              equals: id,
+            },
+          },
+        ],
+      },
+    });
 
-		return result;
-	}
+    return result;
+  }
 
-	async findAllByIdByOffset(id: string, offset: number) {
-		const result = await this.prismaService.matches.findMany({
-			where: {
-				OR: [
-					{
-						winnerID: {
-							equals: id,
-						},
-					},
-					{
-						loserID: {
-							equals: id,
-						},
-					},
-				],
-			},
-			skip: offset,
-			take: 5,
-		});
+  async findVsByIdByOffset(user: ActiveUserData, id: string, offset: number) {
+    const result = await this.prismaService.matches.findMany({
+      where: {
+        OR: [
+          {
+            winnerID: {
+              equals: user.sub,
+            },
+            loserID: {
+              equals: id,
+            },
+          },
+          {
+            winnerID: {
+              equals: id,
+            },
+            loserID: {
+              equals: user.sub,
+            },
+          },
+        ],
+      },
+      skip: offset,
+      take: 5,
+    });
 
-		return result;
-	}
+    return result;
+  }
 
-	async getStats(user: ActiveUserData) {
-		const matches = await this.findAll(user);
-		let highest = 0;
-		let currentSum = 0;
-		let wins = 0;
+  async findAllByIdByOffset(id: string, offset: number) {
+    const result = await this.prismaService.matches.findMany({
+      where: {
+        OR: [
+          {
+            winnerID: {
+              equals: id,
+            },
+          },
+          {
+            loserID: {
+              equals: id,
+            },
+          },
+        ],
+      },
+      skip: offset,
+      take: 5,
+    });
 
-		matches.map((match) => {
-			if (match.winnerID === user.sub) {
-				wins++;
-				currentSum++;
-			} else currentSum = 0;
-			if (currentSum > highest) highest = currentSum;
-		});
+    return result;
+  }
 
-		return {
-			winstreak: highest,
-			winrate: (wins / matches.length) * 100,
-			total: matches.length,
-		};
-	}
+  async getStats(user: ActiveUserData) {
+    const matches = await this.findAll(user);
+    let highest = 0;
+    let currentSum = 0;
+    let wins = 0;
 
-	async getStatsById(id: string) {
-		const matches = await this.findAllById(id);
-		let highest = 0;
-		let currentSum = 0;
-		let wins = 0;
+    matches.map((match) => {
+      if (match.winnerID === user.sub) {
+        wins++;
+        currentSum++;
+      } else currentSum = 0;
+      if (currentSum > highest) highest = currentSum;
+    });
 
-		matches.map((match) => {
-			if (match.winnerID === id) {
-				wins++;
-				currentSum++;
-			} else currentSum = 0;
-			if (currentSum > highest) highest = currentSum;
-		});
+    return {
+      winstreak: highest,
+      winrate: (wins / matches.length) * 100,
+      total: matches.length,
+    };
+  }
 
-		return {
-			winstreak: highest,
-			winrate: (wins / matches.length) * 100,
-			total: matches.length,
-		};
-	}
+  async getStatsById(id: string) {
+    const matches = await this.findAllById(id);
+    let highest = 0;
+    let currentSum = 0;
+    let wins = 0;
 
-	async findOne(id: number) {
-		this.logger.log(`findOne for match with id: ${id}`);
+    matches.map((match) => {
+      if (match.winnerID === id) {
+        wins++;
+        currentSum++;
+      } else currentSum = 0;
+      if (currentSum > highest) highest = currentSum;
+    });
 
-		const result = await this.prismaService.matches.findUnique({
-			where: {
-				id,
-			},
-		});
+    return {
+      winstreak: highest,
+      winrate: (wins / matches.length) * 100,
+      total: matches.length,
+    };
+  }
 
-		this.logger.verbose(result);
-		return result;
-	}
+  async findOne(id: number) {
+    const result = await this.prismaService.matches.findUnique({
+      where: {
+        id,
+      },
+    });
 
-	async getLatest(user: ActiveUserData, take: number) {
-		const result = await this.prismaService.matches.findMany({
-			where: {
-				OR: [
-					{
-						winnerID: {
-							equals: user.sub,
-						},
-					},
-					{
-						loserID: {
-							equals: user.sub,
-						},
-					},
-				],
-			},
-			take,
-		});
+    return result;
+  }
 
-		return result;
-	}
+  async getLatest(user: ActiveUserData, take: number) {
+    const result = await this.prismaService.matches.findMany({
+      where: {
+        OR: [
+          {
+            winnerID: {
+              equals: user.sub,
+            },
+          },
+          {
+            loserID: {
+              equals: user.sub,
+            },
+          },
+        ],
+      },
+      take,
+    });
 
-	async update(id: number, updateMatchDto: UpdateMatchDto) {
-		let result = null;
+    return result;
+  }
 
-		try {
-			result = await this.prismaService.matches.update({
-				where: {
-					id,
-				},
-				data: updateMatchDto,
-			});
-		} catch (error) {
-			this.logger.error(`Attempted to update match id: ${id}`);
-			throw new HttpException("Match doesn't exists", HttpStatus.NOT_FOUND);
-		}
+  async update(id: number, updateMatchDto: UpdateMatchDto) {
+    let result = null;
 
-		this.logger.debug(
-			`update Match ${result.winnerID} vs ${result.loserID} date: ${result.date}`,
-		);
-		return result;
-	}
+    try {
+      result = await this.prismaService.matches.update({
+        where: {
+          id,
+        },
+        data: updateMatchDto,
+      });
+    } catch (error) {
+      this.logger.error(`Attempted to update match id: ${id}`);
+      throw new HttpException("Match doesn't exists", HttpStatus.NOT_FOUND);
+    }
 
-	async remove(id: number) {
-		let result = null;
+    this.logger.debug(
+      `update Match ${result.winnerID} vs ${result.loserID} date: ${result.date}`,
+    );
+    return result;
+  }
 
-		try {
-			result = await this.prismaService.matches.delete({
-				where: {
-					id,
-				},
-			});
-		} catch (error) {
-			this.logger.error(`Attempted to delete match id: ${id}`);
-			throw new HttpException("Match doesn't exist", HttpStatus.NOT_FOUND);
-		}
+  async remove(id: number) {
+    let result = null;
 
-		this.logger.debug(
-			`delete Match ${result.winnerID} vs ${result.loserID} date: ${result.date}`,
-		);
-		return result;
-	}
+    try {
+      result = await this.prismaService.matches.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      this.logger.error(`Attempted to delete match id: ${id}`);
+      throw new HttpException("Match doesn't exist", HttpStatus.NOT_FOUND);
+    }
+
+    this.logger.debug(
+      `delete Match ${result.winnerID} vs ${result.loserID} date: ${result.date}`,
+    );
+    return result;
+  }
 }
