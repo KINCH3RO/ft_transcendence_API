3e8c293ca7e492cd8bafb2d691b7568b5e2f8f74
Hatim Rachid <hat83203@gmail.com>
2023-12-05 23:31:17 +0100

    fixing lobby disconnect

--- src/res/web-socket/gateways/main.gateway.ts
+++ src/res/web-socket/gateways/main.gateway.ts
@@ -1,11 +1,11 @@
 import { UseFilters, UseGuards } from '@nestjs/common';
 import {
-  BaseWsExceptionFilter,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
+	BaseWsExceptionFilter,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { TokenGuard } from '../token.guard';
@@ -23,93 +23,91 @@ import { StatsService } from 'src/game/stats.service';
 @UseGuards(TokenGuard)
 @WebSocketGateway({ cors: true, transports: ['websocket'] })
 export class MainGate implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(
-    private readonly webSocketService: WebSocketService,
-    private lobbyService: LobbyService,
-    private matchmakingService: MatchmakingService,
-    private statsService: StatsService,
-  ) {}
+	constructor(
+		private readonly webSocketService: WebSocketService,
+		private lobbyService: LobbyService,
+		private matchmakingService: MatchmakingService,
+		private statsService: StatsService,
+	) { }
 
-  @WebSocketServer()
-  io: Server;
+	@WebSocketServer()
+	io: Server;
 
-  handleConnection(client: Socket, ...args: any[]) {
-    // console.log(client);
-    console.log('=> A socket has connected with ID: ', client.id);
-    if (!client.handshake.query.userId) {
-      client.disconnect();
-      return false;
-    }
-    this.webSocketService.getUserFromToken(
-      client.handshake.query.userId as string,
-      (userData) => {
-        client.join(userData.sub);
-        // join all user channels (chat)
-        this.webSocketService
-          .getUserChannels(userData.sub)
-          .then((channels) => client.join(channels));
-        //emit event to other user that the user is connected
-        this.webSocketService.userConnected(userData.sub, client.id, () => {
-          client.broadcast.emit('connected', userData.sub);
-        });
-        //emit queue event
-        const queueData: queueData = this.matchmakingService.getPlayerInQ(
-          userData.sub,
-        );
-        if (queueData) client.emit('enterQueue', queueData);
-        let lobby: Lobby = this.lobbyService.getLobby(userData.sub);
-        //lobby stuff
-        if (!lobby) return;
-        client.join(lobby.id);
-        lobby.isOwner = lobby.owner == userData.sub;
-        client.emit('lobbyData', lobby);
-      },
-      (err) => {
-        console.log(err);
-        client.disconnect();
-      },
-    );
-  }
-  handleDisconnect(client: Socket) {
-    console.log('=> A socket has disconnected with ID: ', client.id);
-    if (!client.handshake.query.userId) return false;
-    this.webSocketService.userDisconnected(
-      client.handshake.query.userId as string,
-      client.id,
-      async (userID) => {
-        client.broadcast.emit('disconnected', userID);
-        let lobby: Lobby = this.lobbyService.getLobby(userID);
-        //matchmaking suff
-        this.matchmakingService.removePlayer(userID);
-        //lobby stuff
-        if (!lobby) return;
-        let oppnentdID = lobby.players.find((x) => x.id != userID).id;
-        if (lobby.lobbySate !== 'starting') {
-          clearInterval(lobby.gameInterval);
-          if (oppnentdID == lobby.players[0].id) lobby.gameData.score[0] = 5;
-          else lobby.gameData.score[1] = 5;
-          lobby.gameData.timer =
-            (Date.now() - lobby.gameData.gameStartDate) / 1000;
-          const [winner, loser] = await this.statsService.saveGame(lobby);
-          this.io.to(winner.id).emit('gameEnd', { lobby, rewards: winner });
-          this.io.to(loser.id).emit('gameEnd', { lobby, rewards: loser });
-        }
-		this.io.to(lobby.id).emit('leaveLobby');
-        this.webSocketService.getSockets(oppnentdID).forEach((socketID) => {
-          this.io.sockets.sockets.get(socketID).leave(lobby.id);
-        });
-        this.lobbyService.deleteLobby(lobby.id);
-        //end lobby
-        //
-      },
-    );
-  }
+	handleConnection(client: Socket, ...args: any[]) {
+		// console.log(client);
+		console.log('=> A socket has connected with ID: ', client.id);
+		if (!client.handshake.query.userId) {
+			client.disconnect();
+			return false;
+		}
+		this.webSocketService.getUserFromToken(
+			client.handshake.query.userId as string,
+			(userData) => {
+				client.join(userData.sub);
+				// join all user channels (chat)
+				this.webSocketService
+					.getUserChannels(userData.sub)
+					.then((channels) => client.join(channels));
+				//emit event to other user that the user is connected
+				this.webSocketService.userConnected(userData.sub, client.id, () => {
+					client.broadcast.emit('connected', userData.sub);
+				});
+				//emit queue event
+				const queueData: queueData = this.matchmakingService.getPlayerInQ(
+					userData.sub,
+				);
+				if (queueData) client.emit('enterQueue', queueData);
+				let lobby: Lobby = this.lobbyService.getLobby(userData.sub);
+				//lobby stuff
+				if (!lobby) return;
+				client.join(lobby.id);
+				lobby.isOwner = lobby.owner == userData.sub;
+				client.emit('lobbyData', lobby);
+			},
+			(err) => {
+				console.log(err);
+				client.disconnect();
+			},
+		);
+	}
+	handleDisconnect(client: Socket) {
+		console.log('=> A socket has disconnected with ID: ', client.id);
+		if (!client.handshake.query.userId) return false;
+		this.webSocketService.userDisconnected(
+			client.handshake.query.userId as string,
+			client.id,
+			async (userID) => {
+				client.broadcast.emit('disconnected', userID);
+				let lobby: Lobby = this.lobbyService.getLobby(userID);
+				//matchmaking suff
+				this.matchmakingService.removePlayer(userID);
+				//lobby stuff
+				if (!lobby) return;
+				let oppnentdID = lobby.players.find((x) => x.id != userID).id;
+				if (lobby.lobbySate == 'ingame') {
+					if (oppnentdID == lobby.players[0].id) lobby.gameData.score[0] = 5;
+					else lobby.gameData.score[1] = 5;
+					lobby.gameData.timer =
+						(Date.now() - lobby.gameData.gameStartDate) / 1000;
+					const [winner, loser] = await this.statsService.saveGame(lobby);
+					this.io.to(winner.id).emit('gameEnd', { lobby, rewards: winner });
+				}
+				this.io.to(lobby.id).emit('leaveLobby');
+				this.webSocketService.getSockets(oppnentdID).forEach((socketID) => {
+					this.io.sockets.sockets.get(socketID).leave(lobby.id);
+				});
+				this.lobbyService.deleteLobby(lobby.id);
+				//end lobby
+				//
+			},
+		);
+	}
 
-  @SubscribeMessage('presence')
-  handlePresence(socket: Socket, data: BodyData) {
-    this.webSocketService.setPresenceState(data.sender.id, data.data);
-    socket.broadcast.emit('presence', data);
+	@SubscribeMessage('presence')
+	handlePresence(socket: Socket, data: BodyData) {
+		this.webSocketService.setPresenceState(data.sender.id, data.data);
+		socket.broadcast.emit('presence', data);
 
-    // console.log(this.webSocketService.onlineUsers[data.sender.id]);
-  }
+		// console.log(this.webSocketService.onlineUsers[data.sender.id]);
+	}
 }
--- src/res/web-socket/types/lobby.interface.ts
+++ src/res/web-socket/types/lobby.interface.ts
@@ -6,70 +6,69 @@ import UserData from './user-data.interface';
 import { UUID } from 'crypto';
 
 export default interface Lobby {
-  id?: UUID;
-  players: [UserData, UserData];
-  ranked: boolean;
-  mode: GameMode;
-  owner: string;
-  queueLobby: boolean;
-  isOwner?: boolean;
-  lobbySate: 'ingame' | 'idle' | 'starting' | 'finishing';
-  gameData: GameData;
-  gameInterval: NodeJS.Timeout;
-  skins?: Repo[];
+	id?: UUID;
+	players: [UserData, UserData];
+	ranked: boolean;
+	mode: GameMode;
+	owner: string;
+	queueLobby: boolean;
+	isOwner?: boolean;
+	lobbySate: 'ingame' | 'idle' | 'starting' | 'finishing';
+	gameData: GameData;
+	skins?: Repo[];
 }
 
 export interface LobbyCreate {
-  players: [string, string];
-  mode?: GameMode;
-  queueLobby?: boolean;
-  ranked?: boolean;
-  lobbySate?: 'idle' | 'ingame' | 'starting';
-  skins?: Repo[];
+	players: [string, string];
+	mode?: GameMode;
+	queueLobby?: boolean;
+	ranked?: boolean;
+	lobbySate?: 'idle' | 'ingame' | 'starting';
+	skins?: Repo[];
 }
 
 export interface GameData {
-  paddle1: Paddle;
-  paddle2: Paddle;
-  ball: Ball;
-  score: number[];
-  scoreUpdated: boolean;
-  resourcesUpdated: boolean;
-  gameStartDate: number;
-  timer?: number;
-  spawner?: GraviraSpawner;
-  spellWeaver?: SpellWeaverEntity;
-  achievements: AchievementUnlock[];
+	paddle1: Paddle;
+	paddle2: Paddle;
+	ball: Ball;
+	score: number[];
+	scoreUpdated: boolean;
+	resourcesUpdated: boolean;
+	gameStartDate: number;
+	timer?: number;
+	spawner?: GraviraSpawner;
+	spellWeaver?: SpellWeaverEntity;
+	achievements: AchievementUnlock[];
 }
 
 export interface AchievementUnlock {
-  stunnedSavior: boolean;
-  speedySlipup: boolean;
-  deleteGame: boolean;
-  imperturbable: boolean;
+	stunnedSavior: boolean;
+	speedySlipup: boolean;
+	deleteGame: boolean;
+	imperturbable: boolean;
 }
 
 export interface Paddle {
-  x: number;
-  y: number;
-  mana: number | null;
-  isCasting: boolean;
-  isStunned: boolean;
-  isUP: boolean;
-  isDown: boolean;
-  numberPressed: string | null;
-  height: number;
-  speed: number;
-  castDuration: number;
-  stunDuration: number;
-  homingStunOrbs: { x: number; y: number; collided: boolean }[];
+	x: number;
+	y: number;
+	mana: number | null;
+	isCasting: boolean;
+	isStunned: boolean;
+	isUP: boolean;
+	isDown: boolean;
+	numberPressed: string | null;
+	height: number;
+	speed: number;
+	castDuration: number;
+	stunDuration: number;
+	homingStunOrbs: { x: number; y: number; collided: boolean }[];
 }
 
 export interface Ball {
-  x: number;
-  y: number;
-  xDirection: number;
-  yDirection: number;
-  xSpeed: number;
-  ySpeed: number;
+	x: number;
+	y: number;
+	xDirection: number;
+	yDirection: number;
+	xSpeed: number;
+	ySpeed: number;
 }
